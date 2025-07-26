072c6c94fc3e327c1619b0fa723ecd9ba955011d
Darul-Uloom-Khademul-Islam-Madrasha <khadimulislammadrasa5@gmail.com>
2025-07-26 16:31:08 +0600

    Update script.js with quote posting

--- script.js
+++ script.js
@@ -157,3 +157,172 @@ profileSVG.addEventListener("click", () => {
       '<svg fill="crimson" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2024 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg>';
   }
 });
+
+// on Submitting a profile
+const editorLogger = document.querySelector(".editor p span");
+const editor = document.querySelector(".editor");
+const editorLog = document.querySelector(".editor div");
+const signOutPrompt = document.querySelector(".signOutPrompt");
+const submitBtn = document.querySelector('.btn[type="submit"]');
+const inputEmail = document.querySelector('input[type="email"]');
+const inputPass = document.querySelector('input[name="password"]');
+const profileNotice = document.querySelector(".profileNotice");
+const quoteEditor = document.querySelector(".quoteEditor");
+const quoteEditorWindow = document.querySelector(".quoteEditorWindow");
+const profiles = {
+  email: [
+    "khadimulislammadrasa5.com",
+    "alaminkhan00710@gmail.com",
+    "code.islah@gmail.com",
+  ],
+  password: ["dukim1969", "password123", "password456"],
+};
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (inputEmail.value === "" || inputPass.value === "") {
+    inputEmail.style.outline = "3px solid crimson";
+    inputPass.style.outline = "3px solid crimson";
+    inputEmail.style.animation = "warning 1s ease-out infinite";
+    inputPass.style.animation = "warning 1s ease-out infinite";
+
+    setTimeout(() => {
+      inputEmail.style.outline = "";
+      inputPass.style.outline = "";
+      inputEmail.style.animation = "";
+      inputPass.style.animation = "";
+    }, 1000);
+  } else if (
+    profiles.email.includes(inputEmail.value) &&
+    profiles.password.includes(inputPass.value) &&
+    profiles.email.indexOf(inputEmail.value) ===
+      profiles.password.indexOf(inputPass.value)
+  ) {
+    signIn();
+  } else {
+    invaildUser();
+  }
+});
+
+function signIn() {
+  notice(0, "rgba(68, 167, 68, 1)", "fadeIn 500ms linear");
+  displayTrue(profileNotice);
+  quoteEditor.style.display = "grid";
+  editor.style.display = "flex";
+  editorLogger.innerHTML = `- ${inputEmail.value}`;
+  profileSVG.classList.add("remove-pointer");
+  setTimeout(() => {
+    displayFalse(profileNotice, profileFormWrapper);
+    profile.classList.remove("toggleProfile");
+    profileSVG.innerHTML = `<svg fill="rgba(68, 167, 68, 1)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2024 Fonticons, Inc. --><path d="M399 384.2C376.9 345.8 335.4 320 288 320l-64 0c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/></svg>`;
+  }, 1000);
+}
+
+function invaildUser() {
+  notice(1, "crimson", "fadeIn 500ms linear");
+  displayTrue(profileNotice);
+  setInterval(() => {
+    displayFalse(profileNotice);
+  }, 1000);
+}
+
+function signOut() {
+  const YES = document.querySelector(".signOutPrompt *:nth-child(2)");
+  const NO = document.querySelector(".signOutPrompt *:nth-child(3)");
+
+  signOutPrompt.style.display = "flex";
+
+  YES.addEventListener("click", () => {
+    notice(2, "rgba(68, 167, 68, 1)", "fadeIn 500ms linear");
+    displayTrue(profileNotice);
+    profileSVG.classList.remove("remove-pointer");
+
+    setTimeout(() => {
+      displayFalse(profileNotice, editor, quoteEditor, signOutPrompt);
+      profileSVG.innerHTML = `<svg fill="gray" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2024 Fonticons, Inc. --><path d="M399 384.2C376.9 345.8 335.4 320 288 320l-64 0c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/></svg>`;
+    }, 1000);
+  });
+
+  NO.addEventListener("click", () => {
+    displayFalse(signOutPrompt);
+  });
+}
+
+function displayTrue(...elem) {
+  elem.forEach((e) => {
+    e.style.display = "block";
+  });
+}
+
+function displayFalse(...elem) {
+  elem.forEach((e) => {
+    e.style.display = "none";
+  });
+}
+
+function notice(inner, bg, animation) {
+  const logText = ["Log In Succcessful", "No User Found", "Singed Out"];
+  profileNotice.innerHTML = logText[inner];
+  profileNotice.style.background = bg;
+  profileNotice.style.animation = animation;
+
+  setTimeout(() => {
+    profileNotice.style.animation = "";
+  }, 1000);
+}
+
+function displayToggler() {
+  quoteEditorWindow.classList.toggle("d-none");
+}
+
+// show/hide password
+function showHide() {
+  if (inputPass.type === "password") {
+    inputPass.type = "text";
+  } else {
+    inputPass.type = "password";
+  }
+}
+
+// editor setup
+
+const editor__signOut = document.querySelector(".editor__signOut");
+const editor__info = document.querySelector(".editor__info");
+
+editor__signOut.addEventListener("click", () => {
+  signOut();
+});
+
+quoteEditor.addEventListener("click", () => {
+  displayToggler();
+});
+
+editor__info.addEventListener("click", () => {
+  const current = editorLog.getAttribute("data-log");
+  const content = `${inputEmail.value} \n ${inputPass.value}`;
+  editorLog.classList.toggle("active");
+
+  if (current === content) {
+    editorLog.setAttribute("data-log", "");
+  } else {
+    editorLog.setAttribute("data-log", content);
+  }
+});
+
+// POST & GET for updateJSON
+
+document.getElementById("jsonForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const response = await fetch("/.netlify/functions/updateJson", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      quote__header: document.getElementById("quote__header").value,
+      quote__lead: document.getElementById("quote__lead").value,
+      quote__text: document.getElementById("quote__text").value,
+      quote__ref: document.getElementById("quote__ref").value,
+    }),
+  });
+  alert(await response.text());
+});
